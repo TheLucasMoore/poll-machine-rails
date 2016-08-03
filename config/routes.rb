c29759a7d8d31b49de97a16112b088859f1104f4
TheLucasMoore <lucasedwardmoore@gmail.com>
2016-08-03 15:51:29 -0600

    generated controllers/views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,20 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'static/home'
+  get 'votes/create'
 
+  get 'questions/index'
+
+  get 'questions/show'
+
+  get 'polls/index'
+
+  get 'polls/show'
+
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static#home'
+
+  resources :poll do
+    resources :quesitons
+  end
+  resources :votes, only: [:create]
 end
