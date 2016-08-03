7727add3e0ac4c190f728387cc37df2ed6489d9f
TheLucasMoore <lucasedwardmoore@gmail.com>
2016-08-03 17:02:08 -0600

    rough home page bootstrap and forms

--- app/controllers/polls_controller.rb
+++ app/controllers/polls_controller.rb
@@ -1,7 +1,28 @@
 class PollsController < ApplicationController
   def index
+    @polls = Poll.all
   end
 
   def show
+    @poll = Poll.find(params[:id])
+  end
+
+  def new
+    @poll = Poll.new
+  end
+
+  def create
+    @poll = Poll.new(poll_params)
+    if @poll.save
+      redirect_to poll_path(@poll)
+    else
+      render new
+    end
+  end
+
+  private
+
+  def poll_params
+    require(:poll).permit(:title, :user_id, question_attributes: [:content])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,9 @@
 Rails.application.routes.draw do
-  get 'votes/create'
-
-  get 'questions/index'
-
-  get 'questions/show'
-
-  get 'polls/index'
-
-  get 'polls/show'
-
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static#home'
 
-  resources :poll do
+  resources :polls do
     resources :quesitons
   end
   resources :votes, only: [:create]
