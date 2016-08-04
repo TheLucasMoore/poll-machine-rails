ee0d7b3fda52395ad54f2442579c7756bf8a12cf
TheLucasMoore <lucasedwardmoore@gmail.com>
2016-08-04 10:51:23 -0600

    AJAX voting with verification of one vote

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,4 +1,13 @@
 class VotesController < ApplicationController
   def create
+    question_id = params["question"]
+    @question = Question.find(question_id)
+    @vote = Vote.create(question: @question, user: current_user)
+  end
+
+  private
+
+  def vote_params
+    params.require(:votes).permit(:question)
   end
 end
