2202d94743441eaf6e9bafbcac6da169ed6f04ef
TheLucasMoore <lucasedwardmoore@gmail.com>
2016-08-04 14:59:23 -0600

    ChartJS uses real data

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,5 +1,10 @@
 class ChartsController < ApplicationController
   def index
-    # @chart = Chart.find(params[:id])
+    @poll = Poll.find(params[:poll_id])
+    @questions = @poll.questions
+    respond_to do |format|
+      format.html {render :index}
+      format.json {render json: @questions}
+    end
   end
 end
--- app/helpers/charts_helper.rb
+++ app/helpers/charts_helper.rb
@@ -1,2 +1,10 @@
 module ChartsHelper
+
+  def label_builder(questions)
+    question_array = []
+      questions.each do |question|
+        question_array.push(question.content.to_s)
+      end
+    question_array
+  end
 end
