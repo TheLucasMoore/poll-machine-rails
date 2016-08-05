b85cea59f465d8d23ed901f604ea507ef27bb7f4
TheLucasMoore <lucasedwardmoore@gmail.com>
2016-08-04 19:59:23 -0600

    validates date_time

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -5,4 +5,6 @@ class Poll < ApplicationRecord
   accepts_nested_attributes_for :questions
   validates :user_id, presence: true
   validates :title, uniqueness: true, presence: true
+  validates :end_time, presence: true
+
 end
