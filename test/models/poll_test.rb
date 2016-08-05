681e55b1da97499c77518e2c9f0346264181dca5
TheLucasMoore <lucasedwardmoore@gmail.com>
2016-08-04 23:55:38 -0600

    Revise home page styles

--- test/models/poll_test.rb
+++ test/models/poll_test.rb
@@ -2,10 +2,4 @@ require 'test_helper'
 
 class PollTest < ActiveSupport::TestCase
 
-  it "should require a poll title" do
-    poll = Poll.create(:title => "")
-    poll.valid?
-    poll.errors.should have_key(:title)
-  end
-  
 end
--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class VoteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  describe "Voting" do
+    it "has a user"
+    it "has a question"
+    it "belongs to a poll"
+    it "does not allow a user to vote on the same poll twice!"
+  end
 end
